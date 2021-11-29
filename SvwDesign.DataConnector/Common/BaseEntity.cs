b3309feea26abf55701a6d974e79825de2cb926f
Susan von Wielligh <susan@svwdesign.com>
2021-11-29 13:05:33 +0200

    Fix Base Entity

--- SvwDesign.DataConnector/Common/BaseEntity.cs
+++ SvwDesign.DataConnector/Common/BaseEntity.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SvwDesign.DataConnector.Common
 {
     public abstract class BaseEntity<TId>
     {
         public TId Id { get; set; }
+
+        [ConcurrencyCheck]
         public byte[]? Version { get; set; }
+
         public bool Active { get; set; } = true;
 
         public DateTime Created { get; set; }
-        public string? CreatedBy { get; set; }
-        public DateTime? LastModified { get; set; }
-        public string? LastModifiedBy { get; set; }
 
+        public string? CreatedBy { get; set; }
+
+        public DateTime? LastModified { get; set; }
+
+        public string? LastModifiedBy { get; set; }
 
         public List<BaseDomainEvent> Events = new(); 
     }
